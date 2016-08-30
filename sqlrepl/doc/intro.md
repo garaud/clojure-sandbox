150a632b187ec0716f8121f5ae2a7d060b7f8a7f
Damien Garaud <damien.garaud@gmail.com>
2016-08-30 22:27:25 +0200

    add sqlrepl

---
+++ sqlrepl/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to sqlrepl
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ sqlrepl/test/sqlrepl/core_test.clj
@@ -0,0 +1,7 @@
+(ns sqlrepl.core-test
+  (:require [clojure.test :refer :all]
+            [sqlrepl.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

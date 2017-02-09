f3be964e8288131f87541b22759a24c010fe623b
Damien Garaud <damien.garaud@gmail.com>
2017-02-09 20:50:13 +0100

    4clojure: add some solutions/problems

--- for-clojure/src/for_clojure/core.clj
+++ for-clojure/src/for_clojure/core.clj
@@ -1,11 +1,32 @@
 (ns for-clojure.core
   (:gen-class))
 
+;; problem 15
 (defn double-down [x]
   "double x"
   (* x 2))
 
+;; problem 19
+(defn last-element [col]
+  "get the last element"
+  (first (reverse col)))
+
+;; problem 20
+(defn penultimate [col]
+  "get the last but one element"
+  (nth (reverse col) 1))
+
+;; problem 21
+(defn element-at [col n]
+  "return the nth element (nth is a restriction)"
+  (first (drop n col)))
+
+;; problem 22
+(defn element-nb [col]
+  "count the number of elements (count is a restriction)"
+  (reduce (fn [acc v] (inc acc)) 0 col))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello 4Clojure!!"))
+  (println "Hello 4Clojure!! lein test to launch all tests"))
--- for-clojure/test/for_clojure/core_test.clj
+++ for-clojure/test/for_clojure/core_test.clj
@@ -8,3 +8,30 @@
     (is (= (double-down 2) 4))
     (is (= (double-down -3) -6))
     (is (= (double-down 0) 0))))
+
+(deftest test-problem-19-last-element
+  (testing "last element - problem 19"
+    (is (= (last-element [1 2 3 4 5]) 5))
+    (is (= (last-element '(5 4 3)) 3))
+    (is (= (last-element ["b" "c" "d"]) "d"))))
+
+(deftest test-problem-20-penultimate-element
+  (testing "penultimate element (last but one)"
+    (is (= (penultimate (list 1 2 3 4 5)) 4))
+    (is (= (penultimate ["a" "b" "c"]) "b"))
+    (is (= (penultimate [[1 2] [3 4]]) [1 2]))))
+
+(deftest test-problem-21-nth-element
+  (testing "Nth element - problem 21 *special restrictions*: 'nth'"
+    (is (= (element-at '(4 5 6 7) 2) 6))
+    (is (= (element-at [:a :b :c] 0) :a))
+    (is (= (element-at [1 2 3 4] 1) 2))
+    (is (= (element-at '([1 2] [3 4] [5 6]) 2) [5 6]))))
+
+(deftest test-problem-22-count-a-sequence
+  (testing "count a sequence -problem 22 *special restrictions*: 'count'"
+    (is (= (element-nb '(1 2 3 3 1)) 5))
+    (is (= (element-nb "Hello World") 11))
+    (is (= (element-nb [[1 2] [3 4] [5 6]]) 3))
+    (is (= (element-nb '(13)) 1))
+    (is (= (element-nb '(:a :b :c)) 3))))
